abb04c3a75b2713735c3ce113589695cc1c625e8
Alex0216Lee <alex99925759198@gmail.com>
2024-12-16 23:25:02 +0800

    test-6

--- REC/app.py
+++ REC/app.py
@@ -74,4 +74,4 @@ def search_dhr():
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
